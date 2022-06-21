fb74e27e0601037fd98944c5863e90d731936a57
Dejan Simic <dejansimic@gmx.net>
2022-06-21 18:46:00 +0200

    Finalize reset logic

--- index.js
+++ index.js
@@ -23,70 +23,88 @@ let playerCount = 0;
 let computerCount = 0;
 
 const container = document.querySelector('.container');
-let display = document.createElement('div');
+const display = document.createElement('div');
+let logger = document.createElement('h3');
 display.classList.add('display');
-container.appendChild(display);
+display.classList.add('column-center');
+display.appendChild(logger);
+
+
+
+let reset = true;
 
 
 //one round of rock paper scissors and return result
 function playRPS (playerSelection, computerSelection) {
+    if(reset) {
+        container.appendChild(display);
+        reset = false;
+    }
     const playerSelectionFormatted = playerSelection.toLowerCase();
 
     if (playerSelectionFormatted == 'rock') {
         if (computerSelection == 'rock') {
-            display.textContent = `${playerSelection}  vs. ${computerSelection} \n 
+            logger.textContent = `${playerSelection}  vs. ${computerSelection}:
                                     No winner! Score stays ${playerCount}:${computerCount}!`;
         }
         else if (computerSelection == 'paper') {
             computerCount += 1;
-            display.textContent = `${playerSelection}  vs. ${computerSelection} \n 
+            logger.textContent = `${playerSelection}  vs. ${computerSelection}: 
                                     Computer wins! New score: ${playerCount}:${computerCount}!`;
         }
-        playerCount += 1;
-        display.textContent = `${playerSelection}  vs. ${computerSelection} \n 
-                                You win! New score: ${playerCount}:${computerCount}!`;
+        else {
+            playerCount += 1;
+            logger.textContent = `${playerSelection}  vs. ${computerSelection}: 
+                                    You win! New score: ${playerCount}:${computerCount}!`;
+        }
 
     }
     else if (playerSelectionFormatted == 'paper') {
         if (computerSelection == 'rock') {
             playerCount += 1;
-            display.textContent = `${playerSelection}  vs. ${computerSelection} \n 
+            logger.textContent = `${playerSelection}  vs. ${computerSelection}:
                                     You win! New score: ${playerCount}:${computerCount}!`;
         }
         else if (computerSelection == 'paper') {
-            display.textContent = `${playerSelection}  vs. ${computerSelection} \n 
+            logger.textContent = `${playerSelection}  vs. ${computerSelection}:
                                     No winner! Score stays ${playerCount}:${computerCount}!`
         }
-        computerCount += 1;
-        display.textContent = `${playerSelection}  vs. ${computerSelection} \n 
-                                Computer wins! New score: ${playerCount}:${computerCount}!`;
+        else {
+            computerCount += 1;
+            logger.textContent = `${playerSelection}  vs. ${computerSelection}:
+                                    Computer wins! New score: ${playerCount}:${computerCount}!`;
+        }
 
     }
     else if (playerSelectionFormatted == 'scissors'){
         if (computerSelection == 'rock') {
             computerCount += 1;
-            display.textContent = `${playerSelection}  vs. ${computerSelection} \n
+            logger.textContent = `${playerSelection}  vs. ${computerSelection}:
                                     Computer wins! New score: ${playerCount}:${computerCount}!`; 
         }
         else if (computerSelection == 'paper') {
             playerCount += 1;
-            display.textContent = `${playerSelection}  vs. ${computerSelection} \n
+            logger.textContent = `${playerSelection}  vs. ${computerSelection}:
                                     You win! New score: ${playerCount}:${computerCount}!`;
         }
-        display.textContent = `${playerSelection}  vs. ${computerSelection} \n
+        else {
+            logger.textContent = `${playerSelection}  vs. ${computerSelection}:
                                 No winner! Score stays ${playerCount}:${computerCount}!`;
+        }
     }
     else {
-        display.textContent = 'Please enter one of rock, paper or scissors!';
+        logger.textContent = 'Please enter one of rock, paper or scissors!';
     }
     if(playerCount == 3 || computerCount == 3) {
         if(playerCount == 3) {
             endGame();
-            display.textContent = `Best of 5 is over! You win with the score ${playerCount}:${computerCount}!`;
+            logger.textContent = `Best of 5 is over! You win with the score ${playerCount}:${computerCount}!`;
+            addPlayAgain();
         }
         else {
             endGame();
-            display.textContent = `Best of 5 is over! Computer wins with the score ${playerCount}:${computerCount}!`;
+            logger.textContent = `Best of 5 is over! Computer wins with the score ${playerCount}:${computerCount}!`;
+            addPlayAgain();
         }
     }
 }
@@ -101,12 +119,9 @@ function playRPS (playerSelection, computerSelection) {
 const start = document.querySelector('.start-btn');
 start.addEventListener('click', () => {initGame()});
 
-/**
- * 
- */
- const rock = document.createElement('button');
- const scissors = document.createElement('button');
- const paper = document.createElement('button');
+const rock = document.createElement('button');
+const scissors = document.createElement('button');
+const paper = document.createElement('button');
 
 function addOptions() {
     const buttons = document.querySelector('.buttons');
@@ -143,6 +158,23 @@ function endGame() {
     rock.remove();
     paper.remove();
     scissors.remove();
+}
+
+function addPlayAgain() {
+    const playAgain = document.createElement('button');
+    playAgain.classList.add('start-btn');
+    playAgain.textContent = 'Play again?';
+    setTimeout( () => {
+        playAgain.classList.add('playAgain');
+        container.appendChild(playAgain);
+    }, 750);
+    playAgain.addEventListener('click', () => {
+        reset = true;
+        display.remove();
+        initGame()
+        playerCount = 0;
+        computerCount = 0;
+    });
 }
 
 rock.addEventListener('click', () => {playRPS(rock.innerText, computerPlay())});
